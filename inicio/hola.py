85786714a82071d1db903b03134f85084f5cbb0e
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-09-20 12:16:04 +0200

    first commit

---
+++ inicio/hola.py
@@ -0,0 +1,24 @@
+print ("""Hola mundo,
+       qué tal?""")
+
+a= 5
+b=4.5
+c="hola"
+
+#res= input ("Introduzca un número: ")
+#print(res)
+
+#no hay double en python
+a1= float (input ("Introduzca un número: "))
+a2= float (input ("Introduzca un número: "))
+suma=a1+a2
+print(suma)
+
+num= int (input("Número: "))
+
+if num%2==0:
+    print ("Es par")
+
+else: 
+    print ("No es par")
+
