56c3fd89c8aedb0d931f182cd8f32195cd70e0c9
IsaKatha98 <isabel.loerzer@iesnervion.es>
2023-09-25 13:27:53 +0200

    C3

---
+++ inicio/ejercicios/Ejercicio09.py
@@ -0,0 +1,6 @@
+def pinta (num1, num2):
+    if num1>num2:
+        num1, num2=num2, num1 
+
+    for i in range (num1, num2+1):
+        print(i)
---
+++ inicio/ejercicios/main.py
@@ -0,0 +1,6 @@
+from Ejercicio09 import *
+
+num1= int (input ("introduzca un número:"))
+num2= int (input("otro número: "))
+
+pinta(num1, num2)
